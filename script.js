7f7d6562d8e8ddf358abb1caadce210ac97d718c
10bd <musicandvideogames@hotmail.com>
2015-05-07 15:11:27 -0700

    renamed levelOne image, working out logic and implementing it into script.js

--- script.js
+++ script.js
@@ -11,59 +11,76 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
-// name of our game
-var FF = {
+
 
 
     // set up some initial values
-    WIDTH: 720,
-    HEIGHT: 480,
+    var WIDTH = 720;
+    var HEIGHT = 480;
     // we'll set the rest of these
     // in the init function
-    RATIO: null,
-    currentWidth: null,
-    currentHeight: null,
-    canvas: null,
-    ctx: null,
+    var RATIO = null;
+    var currentWidth = null;
+    var currentHeight = null;
+    var canvas = null;
+    var ctx = null;
 
-    scale: 1,
+    var scale = 1;
 
-    offset: { top: 0, left: 0 },
+    //array variables
+    var empty = 0;
+    var boat = 1;
+    var mine = 2;
+    var treasure = 3;
+    var goal = 4;
+    var bar = 9;
+    
+    //boat directions
+    var left = 5;
+    var up = 6;
+    var right = 7;
+    var down = 8;
+    
+    var level1 = [[bar, bar, bar, bar],
+              [bar, boat, treasure, bar],
+              [bar, mine, goal, bar],
+              [bar, bar, bar, bar]];
 
-    init: function () {
+
+    function init() {
 
         // the proportion of width to height
-        FF.RATIO = FF.WIDTH / FF.HEIGHT;
+        RATIO = WIDTH / HEIGHT;
         // these will change when the screen is resized
-        FF.currentWidth = FF.WIDTH;
-        FF.currentHeight = FF.HEIGHT;
+        window.currentWidth = window.WIDTH;
+        window.currentHeight = window.HEIGHT;
         // this is our canvas element
-        FF.canvas = document.getElementsByTagName('canvas')[0];
+        window.canvas = document.getElementsByTagName('canvas')[0];
         // setting this is important
         // otherwise the browser will
         // default to 320 x 200
-        FF.canvas.width = FF.WIDTH;
-        FF.canvas.height = FF.HEIGHT;
+        window.canvas.width = window.WIDTH;
+        window.canvas.height = window.HEIGHT;
         // the canvas context enables us to 
         // interact with the canvas api
-        FF.ctx = FF.canvas.getContext('2d');
+        window.ctx = window.canvas.getContext('2d');
 
         // we're ready to resize
-        FF.resize();
-        FF.resize();
+        window.resize();
+        window.resize();
 
         // we need to sniff out Android and iOS
         // so that we can hide the address bar in
         // our resize function
-        FF.ua = navigator.userAgent.toLowerCase();
-        FF.android = FF.ua.indexOf('android') > -1 ? true : false;
-        FF.ios = (FF.ua.indexOf('iphone') > -1 || FF.ua.indexOf('ipad') > -1) ?
+        window.ua = navigator.userAgent.toLowerCase();
+        window.android = window.ua.indexOf('android') > -1 ? true : false;
+        window.ios = (window.ua.indexOf('iphone') > -1 || window.ua.indexOf('ipad') > -1) ?
 		true : false;
 
         // listen for clicks
         window.addEventListener('click', function (e) {
             e.preventDefault();
-            FF.Input.set(e);
+            window.Input.set(e);
         }, false);
 
         // listen for touches
@@ -72,7 +89,7 @@ var FF = {
             // the event object has an array
             // named touches; we just want
             // the first touch
-            FF.Input.set(e.touches[0]);
+            window.Input.set(e.touches[0]);
         }, false);
 
         window.addEventListener('touchmove', function (e) {
@@ -89,45 +106,45 @@ var FF = {
         }, false);
 
         
-        FF.ctx.drawImage(document.getElementById("Main"), 0, 0, FF.canvas.width, FF.canvas.height);
+        window.ctx.drawImage(document.getElementById("Main"), 0, 0, window.canvas.width, window.canvas.height);
 
         //drawing invisible images
-        //FF.Draw.circle(100, 100, 50, 'rgba(255,255,0,0)');
+        //window.Draw.circle(100, 100, 50, 'rgba(255,255,0,0)');
 
-        //FF.Draw.rect(0,0,FF.canvas.width,FF.canvas.height*7/8, 'Blue');
+        //window.Draw.rect(0,0,window.canvas.width,window.canvas.height*7/8, 'Blue');
 
 
-        // FF.Draw.rect(0,FF.canvas.height*7/8,FF.canvas.width,FF.canvas.height/8, 'Brown');
-        // FF.Draw.circle(FF.canvas.height*1/16, FF.canvas.height*15/16, FF.canvas.height*1/32, 'rgba(255,255,0,0.5)');
+        // window.Draw.rect(0,window.canvas.height*7/8,window.canvas.width,window.canvas.height/8, 'Brown');
+        // window.Draw.circle(window.canvas.height*1/16, window.canvas.height*15/16, window.canvas.height*1/32, 'rgba(255,255,0,0.5)');
 
-        // FF.Draw.circle(FF.canvas.width - FF.canvas.height*1/16, FF.canvas.height*15/16, FF.canvas.height*1/32, 'rgba(255,255,0,0.5)');
-        // FF.Draw.text('Frantic Frigate', FF.canvas.height*5/32, FF.canvas.height*61/64, 20, '#066');
+        // window.Draw.circle(window.canvas.width - window.canvas.height*1/16, window.canvas.height*15/16, window.canvas.height*1/32, 'rgba(255,255,0,0.5)');
+        // window.Draw.text('Frantic Frigate', window.canvas.height*5/32, window.canvas.height*61/64, 20, '#066');
 
         //how to use an image
         // var levelOne = document.getElementById("levelOne");
-        // FF.ctx.drawImage(levelOne, 0, 0,FF.canvas.width,FF.canvas.height*7/8);
-        // FF.ctx.drawImage(levelOne, 0, FF.canvas.height*7/8,FF.canvas.width,FF.canvas.height*1/8);
-    },
+        // window.ctx.drawImage(levelOne, 0, 0,window.canvas.width,window.canvas.height*7/8);
+        // window.ctx.drawImage(levelOne, 0, window.canvas.height*7/8,window.canvas.width,window.canvas.height*1/8);
+    };
 
-    resize: function () {
+    function resize() {
 
-        FF.currentHeight = window.innerHeight;
+        window.currentHeight = window.innerHeight;
         // resize the width in proportion
         // to the new height
-        FF.currentWidth = FF.currentHeight * FF.RATIO;
+        window.currentWidth = window.currentHeight * window.RATIO;
 
         // this will create some extra space on the
         // page, allowing us to scroll past
         // the address bar, thus hiding it.
-        if (FF.android || FF.ios) {
+        if (window.android || window.ios) {
             document.body.style.height = (window.innerHeight + 50) + 'px';
         }
 
         // set the new canvas style width and height
         // note: our canvas is still 320 x 480, but
         // we're essentially scaling it with CSS
-        FF.canvas.style.width = FF.currentWidth + 'px';
-        FF.canvas.style.height = FF.currentHeight + 'px';
+        window.canvas.style.width = window.currentWidth + 'px';
+        window.canvas.style.height = window.currentHeight + 'px';
 
         // we use a timeout here because some mobile
         // browsers don't fire if there is not
@@ -135,39 +152,37 @@ var FF = {
         window.setTimeout(function () {
             window.scrollTo(0, 1);
         }, 1);
-    }
-
-};
+    };
 
 // abstracts various canvas operations into
 // standalone functions
-FF.Draw = {
+window.Draw = {
 
-    clear: function() {
-        FF.ctx.clearRect(0, 0, FF.WIDTH, FF.HEIGHT);
+    clear: function () {
+        window.ctx.clearRect(0, 0, window.WIDTH, window.HEIGHT);
     },
 
-    rect: function(x, y, w, h, col) {
-        FF.ctx.fillStyle = col;
-        FF.ctx.fillRect(x, y, w, h);
+    rect: function (x, y, w, h, col) {
+        window.ctx.fillStyle = col;
+        window.ctx.fillRect(x, y, w, h);
     },
 
-    circle: function(x, y, r, col) {
-        FF.ctx.fillStyle = col;
-        FF.ctx.beginPath();
-        FF.ctx.arc(x, y, r, 0,  Math.PI * 2, true);
-        FF.ctx.closePath();
-        FF.ctx.fill();
+    circle: function (x, y, r, col) {
+        window.ctx.fillStyle = col;
+        window.ctx.beginPath();
+        window.ctx.arc(x, y, r, 0, Math.PI * 2, true);
+        window.ctx.closePath();
+        window.ctx.fill();
     },
 
-    text: function(string, x, y, size, col) {
-        FF.ctx.font = 'bold '+size+'px Monospace';
-        FF.ctx.fillStyle = col;
-        FF.ctx.fillText(string, x, y);
+    text: function (string, x, y, size, col) {
+        window.ctx.font = 'bold ' + size + 'px Monospace';
+        window.ctx.fillStyle = col;
+        window.ctx.fillText(string, x, y);
     }
-	
+
 
 };
 
-window.addEventListener('load', FF.init, false);
-window.addEventListener('resize', FF.resize, false);
+window.addEventListener('load', window.init, false);
+window.addEventListener('resize', window.resize, false);
