f949eb6ba10df220b95da644f571ea6c3f79e81e
Karan Parmar <karandipparmar@hotmail.com>
2015-05-18 18:36:46 -0700

    All the Menu Buttons and Back Buttons work!

--- menus.js
+++ menus.js
@@ -22,9 +22,10 @@
         play.type = "button";
         play.style.width = '270px';
         play.style.height = '52px';
-        play.style.left = '833px';
+        play.style.left = '750px';
         play.style.top = '100px';
         play.style.background = "none";
+        play.style.border = "none";
         play.style.position = "absolute";
         play.id = "play";
         document.body.appendChild(play);
@@ -39,11 +40,12 @@
 
         var levelselect = document.createElement('input');
         levelselect.type = "button";
-        levelselect.style.width = '482px';
-        levelselect.style.height = '68px';
-        levelselect.style.left = '758px';
-        levelselect.style.top = '268px';
+        levelselect.style.width = '270px';
+        levelselect.style.height = '52px';
+        levelselect.style.left = '746px';
+        levelselect.style.top = '168px';
         levelselect.style.background = "none";
+        levelselect.style.border = "none";
         levelselect.style.position = "absolute";
         levelselect.id = "levelselect";
         document.body.appendChild(levelselect);
@@ -59,15 +61,33 @@
             levelselect.parentNode.removeChild(levelselect)
             instructions.parentNode.removeChild(instructions)
 
+            var back = document.createElement('input');
+            back.type = "button";
+            back.style.width = '85px';
+            back.style.height = '55px';
+            back.style.left = '820px';
+            back.style.top = '360px';
+            back.style.background = "none";
+            back.style.border = "none";
+            back.style.position = "absolute";
+            back.id = "back";
+            document.body.appendChild(back);
+
+            back.onclick = function () {
+                var canvas = document.getElementById("canvas");
+                var context = canvas.getContext("2d");
+                window.location.href="./index.html"
+            };
         };
 
         var highscores = document.createElement('input');
         highscores.type = "button";
-        highscores.style.width = '482px';
-        highscores.style.height = '68px';
-        highscores.style.left = '760px';
-        highscores.style.top = '366px';
+        highscores.style.width = '270px';
+        highscores.style.height = '52px';
+        highscores.style.left = '745px';
+        highscores.style.top = '234px';
         highscores.style.background = "none";
+        highscores.style.border = "none";
         highscores.style.position = "absolute";
         highscores.id = "highscores";
         document.body.appendChild(highscores);
@@ -82,15 +102,34 @@
             highscores.parentNode.removeChild(highscores)
             levelselect.parentNode.removeChild(levelselect)
             instructions.parentNode.removeChild(instructions)
+
+            var back = document.createElement('input');
+            back.type = "button";
+            back.style.width = '142px';
+            back.style.height = '70px';
+            back.style.left = '887px';
+            back.style.top = '404px';
+            back.style.background = "none";
+            back.style.border = "none";
+            back.style.position = "absolute";
+            back.id = "back";
+            document.body.appendChild(back);
+
+            back.onclick = function () {
+                var canvas = document.getElementById("canvas");
+                var context = canvas.getContext("2d");
+                window.location.href="./index.html"
+            };
         };
 
         var instructions = document.createElement('input');
         instructions.type = "button";
-        instructions.style.width = '482px';
-        instructions.style.height = '68px';
-        instructions.style.left = '764px';
-        instructions.style.top = '480px';
+        instructions.style.width = '270px';
+        instructions.style.height = '52px';
+        instructions.style.left = '748px';
+        instructions.style.top = '310px';
         instructions.style.background = "none";
+        instructions.style.border = "none";
         instructions.style.position = "absolute";
         instructions.id = "instructions";
         document.body.appendChild(instructions);
@@ -105,5 +144,23 @@
             highscores.parentNode.removeChild(highscores)
             levelselect.parentNode.removeChild(levelselect)
             instructions.parentNode.removeChild(instructions)
-        };
+
+            var back = document.createElement('input');
+            back.type = "button";
+            back.style.width = '140px';
+            back.style.height = '65px';
+            back.style.left = '840px';
+            back.style.top = '404px';
+            back.style.background = "none";
+            back.style.border = "none";
+            back.style.position = "absolute";
+            back.id = "back";
+            document.body.appendChild(back);
+
+            back.onclick = function () {
+                var canvas = document.getElementById("canvas");
+                var context = canvas.getContext("2d");
+                window.location.href="./index.html"
+            };
+        }
 
