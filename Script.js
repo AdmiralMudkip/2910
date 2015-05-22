7d1fc1c2aa0a110bd5c75d48b36c3050639deeda
10bd <musicandvideogames@hotmail.com>
2015-05-21 23:20:19 -0700

    put boat, mine, and treasure loc on script.js

---
+++ Script.js
@@ -0,0 +1,54 @@
+boat, mine and treasure placements
+
+level 5
+            boatDrawX = 232;
+            boatDrawY = 53;
+            mineX = [1, 2, 3];
+            mineY = [2, 4, 1];
+            treasureX = [1, 4];
+            treasureY = [4, 1];
+            nextLevel = level6;
+
+level 6
+            boatDrawX = 135;
+            boatDrawY = 183;
+            mineX = [1, 2, 3];
+            mineY = [2, 4, 1];
+            treasureX = [1, 5];
+            treasureY = [1, 4];
+            nextLevel = level7;
+
+level 7
+            boatDrawX = 232;
+            boatDrawY = -12;
+            mineX = [1, 2, 2, 2, 3];
+            mineY = [2, 2, 4, 6, 4];
+            treasureX = [1];
+            treasureY = [6];
+            nextLevel = level8;
+
+level 8
+            boatDrawX = 102;
+            boatDrawY = 118;
+            mineX = [2, 3, 3, 4, 4, 5, 6];
+            mineY = [4, 2, 4, 2, 6, 4, 4];
+            treasureX = [3, 3, 7];
+            treasureY = [1, 6, 4];
+            nextLevel = level9;
+
+level 9
+            boatDrawX = 70;
+            boatDrawY = 118;
+            mineX = [1, 2, 2, 4, 5, 6, 7, 7, 8];
+            mineY = [3, 2, 3, 4, 2, 3, 1, 3, 3];
+            treasureX = [1, 8];
+            treasureY = [4, 1];
+            nextLevel = level10;
+
+level 10
+            boatDrawX = 70;
+            boatDrawY = 183;
+            mineX = [1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 7, 8];
+            mineY = [1, 3, 5, 2, 5, 2, 4, 1, 4, 6, 3, 4, 2, 5, 4];
+            treasureX = [4, 7];
+            treasureY = [1, 4];
