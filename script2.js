6cfdd5ebc5476243cce156bb05b941ac19c0aa7c
10bd <musicandvideogames@hotmail.com>
2015-05-22 00:32:47 -0700

    levels 1-10 complete

--- script2.js
+++ script2.js
@@ -237,7 +237,7 @@ window.requestAnimFrame = (function(){
             e.preventDefault();
         }, false);
 
-        startLevel(level6);
+        startLevel(level10);
         //StartMenu();
     };
 
@@ -343,7 +343,6 @@ window.requestAnimFrame = (function(){
         } else if (level == level6) {
             boatDrawX = 135;
             boatDrawY = 50;
-            boatLocX = 1;
             boatLocY = 3;
             mineX = [1, 2, 2, 4, 4, 5, 5, 6];
             mineY = [2, 2, 4, 1, 3, 1, 3, 4];
@@ -351,17 +350,39 @@ window.requestAnimFrame = (function(){
             treasureY = [1, 4];
             nextLevel = level7;
         } else if (level == level7) {
-         
+            boatDrawX = 232;
+            boatDrawY = -12;
+            mineX = [1, 2, 2, 2, 3];
+            mineY = [2, 2, 4, 6, 4];
+            treasureX = [1];
+            treasureY = [6];
             nextLevel = level8;
         } else if (level == level8) {
-            
+            boatDrawX = 102;
+            boatDrawY = -12;
+            boatLocY = 3;
+            mineX = [2, 3, 3, 4, 4, 5, 6];
+            mineY = [4, 2, 4, 2, 6, 4, 4];
+            treasureX = [3, 3, 7];
+            treasureY = [1, 6, 4];
             nextLevel = level9;
         } else if (level == level9) {
-            
+            boatDrawX = 70;
+            boatDrawY = 50;
+            boatLocY = 2;
+            mineX = [1, 2, 2, 4, 5, 6, 7, 7, 8];
+            mineY = [3, 2, 3, 4, 2, 3, 1, 3, 3];
+            treasureX = [1, 8];
+            treasureY = [4, 1];
             nextLevel = level10;
         } else if (level == level10) {
-            
-            
+            boatDrawX = 70;
+            boatDrawY = -12;
+            boatLocY = 4;
+            mineX = [1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 7, 8];
+            mineY = [1, 3, 5, 2, 5, 2, 4, 1, 4, 6, 3, 4, 2, 5, 4];
+            treasureX = [4, 7];
+            treasureY = [1, 4];
         } else {
                
         }
